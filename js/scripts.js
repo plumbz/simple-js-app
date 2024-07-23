98f87e451fa57d5d030291c968d5eca7a6203ec7
plumbz <plumby.eschenbacher@yahoo.com>
2024-07-23 21:36:08 +0200

    created for loop printing pokemon name and height

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
 let pokemonList = [
+    {name: "Bulbasaur",height: 7,types: ["grass", "poison"]},
+    {name: "Charmander",height: 6,types: ["fire"]},
+    {name: "Squirtle",height: 5,types: ["water"]}
+];
+     
+for (let i = 0; i < pokemonList.length; i++)
+{
+    // check if the pokemon is big 
+    if (pokemonList[i].height > 6)  
     {
-      name: "Bulbasaur",
-      height: 7,
-      types: ["grass", "poison"]
-    },
-    {
-      name: "Charmander",
-      height: 6,
-      types: ["fire"]
-    },
-    {
-      name: "Squirtle",
-      height: 5,
-      types: ["water"]
+        // print name, height and note "wow, that's big!" including a new line
+        document.write(pokemonList[i].name +" (height: " + pokemonList[i].height + ") wow, that's big!" + "<br>"); 
     }
-  ];
+    // check if the pokemon is small
+    else if (pokemonList[i].height < 6)
+    {
+        document.write(pokemonList[i].name +" (height: " + pokemonList[i].height +")" + "<br>" );
+    }
+    //otherwise it is normal
+    else 
+    {
+        // print name and height including a new line
+        document.write(pokemonList[i].name +" (height: " + pokemonList[i].height + ") " + "<br>" );
+    }
+}
