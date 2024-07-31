c8f5d46760da3a0084bd319352fae196481b310b
plumbz <plumby.eschenbacher@yahoo.com>
2024-07-31 18:40:44 +0200

    use IIFE to capsule the pokeonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,44 @@
-let pokemonList = [
-    {name: "Bulbasaur",height: 7,types: ["grass", "poison"]},
-    {name: "Charmander",height: 6,types: ["fire"]},
-    {name: "Squirtle",height: 5,types: ["water"]}
-];
+let pokemonRepository = (function (){
+
+    let pokemonList = [
+        {name: "Bulbasaur",height: 7,types: ["grass", "poison"]},
+        {name: "Charmander",height: 6,types: ["fire"]},
+        {name: "Squirtle",height: 5,types: ["water"]}
+    ]
+  
+    function getAll (){
+        return pokemonList;
+    }
+
+   function add (pokemon) { 
+        pokemonList.push(pokemon);
+   }
+
+   return {
+        add: add,
+        getAll: getAll
+
+   }
+        
+       
+})();
 
 function myLoopFunction(pokemonList) 
 {
     // check if the pokemon is big 
     if (pokemonList.height > 6)  
     {
-        console.log(pokemonList.name +" (height: " + pokemonList.height + ") wow, that's big!" ); 
+        // print name, height and note "wow, that's big!" including a new line
+        document.write(pokemonList.name +" (height: " + pokemonList.height + ") wow, that's big!"+ "<br>");
     }
     //otherwise it is normal
     else 
     {
         // print name and height including a new line
-        console.log(pokemonList.name +" (height: " + pokemonList.height + ") " );
+        document.write(pokemonList.name +" (height: " + pokemonList.height + ") " + "<br>" );
     }
 }
 
-pokemonList.forEach(myLoopFunction);
+pokemonRepository.getAll().forEach(myLoopFunction);
+
      
