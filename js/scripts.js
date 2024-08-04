0db09fffa722f774c48be3f170fec41e41d95e09
plumbz <plumby.eschenbacher@yahoo.com>
2024-08-04 23:26:52 +0200

    add styles in css and DOM manipulation

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,56 @@
 let pokemonRepository = (function (){
 
-    let pokemonList = [
+    let repository = [
         {name: "Bulbasaur",height: 7,types: ["grass", "poison"]},
         {name: "Charmander",height: 6,types: ["fire"]},
         {name: "Squirtle",height: 5,types: ["water"]}
-    ]
-  
-    function getAll (){
-        return pokemonList;
+    ];
+
+   function add (pokemon){ 
+      if (
+        typeof pokemon === "object" &&
+        "name" in pokemon &&
+        "height" in pokemon &&
+        "types" in pokemon
+      ){
+        repository.push(pokemon);
+       } else {
+        console.log("pokemon is not correct");
+    }}
+    
+   function getAll (){
+        return repository;
     }
-
-   function add (pokemon) { 
-        pokemonList.push(pokemon);
-   }
-
+    function addListItem (pokemon) {
+        let pokemonList = document.querySelector(".pokemon-List");
+        let listpokemon= document.createElement("Li");
+        let button = document.createElement("button");
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");
+        listpokemon.appendChild(button);
+        pokemonList.appendChild (listpokemon);
+        // Add event listener to the button
+        button.addEventListener("click", function() {
+        showDetails (pokemon);
+        });
+    }  //add showDetails function
+    function showDetails(pokemon) {
+        console.log(pokemon);
+    }
    return {
         add: add,
-        getAll: getAll
-
-   }
-        
-       
+        getAll: getAll,
+        addListItem: addListItem
+    }; 
 })();
 
-function myLoopFunction(pokemonList) 
-{
-    // check if the pokemon is big 
-    if (pokemonList.height > 6)  
-    {
-        // print name, height and note "wow, that's big!" including a new line
-        document.write(pokemonList.name +" (height: " + pokemonList.height + ") wow, that's big!"+ "<br>");
-    }
-    //otherwise it is normal
-    else 
-    {
-        // print name and height including a new line
-        document.write(pokemonList.name +" (height: " + pokemonList.height + ") " + "<br>" );
-    }
-}
+console.log(pokemonRepository.getAll());
 
-pokemonRepository.getAll().forEach(myLoopFunction);
+pokemonRepository.add ({name: "Pikachu", height: 4, types: ["electric"]});
 
-     
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+    
+    pokemonRepository.addListItem (pokemon)
+});
